3f2c42cafa867178d3efe1e7cb468c06c428af60
Hasin Israk Toaha <128958545+toaha63@users.noreply.github.com>
2024-12-14 19:48:52 +0600

    Update BanglaDuino.h

--- src/BanglaDuino.h
+++ src/BanglaDuino.h
@@ -1,21 +1,32 @@
-/*
-
-Library BanglaDuino written by Hasin Israk Toaha in 11/12/2024 .
-Author,Maintainer: Hasin Israk Toaha
+/* 
+Library BanglaDuino written by Hasin Israk Toaha on 11/12/2024.
+Author, Maintainer: Hasin Israk Toaha
 
 Contact: toaha.banaripara@gmail.com
-
 */
 
 #ifndef BANGLADUINO_H
 #define BANGLADUINO_H
 
-#include<Arduino.h>
+#include <Arduino.h>
 
-void printBangla(const char* banglaString);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-void printBanglaln(const char* banglaString);
+void __attribute__((weak)) printBangla(const char* banglaString);
+void __attribute__((weak)) printBanglaln(const char* banglaString);
 
-int lengthOfBanglaString(const char* yourBanglaString);
+String __attribute__((weak)) textToMorse(String input);
+String __attribute__((weak)) morseToText(String morseCode);
+
+int __attribute__((weak)) lengthOfBanglaString(const char* yourBanglaString);
+String __attribute__((weak)) base64_encode(const String &input);
+String __attribute__((weak)) base64_decode(const String &input);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BANGLADUINO_H
